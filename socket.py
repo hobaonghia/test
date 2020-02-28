6d3aa3e0a81abcaa4ead7fc931b5338a35206654
hobaonghia <61495396+hobaonghia@users.noreply.github.com>
2020-02-28 09:54:33 +0700

    Update socket.py

--- socket.py
+++ socket.py
@@ -2,7 +2,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 1234                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
